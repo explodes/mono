e8dd4c7fd6082e4b5c642c90b07b36f691040f23
Evan Leis <evan.explodes@gmail.com>
2018-05-12 17:10:56 -0700

    [statusz] add check command to run a statusz check against a server

---
+++ webs/statusz/check.cc
@@ -0,0 +1,45 @@
+// Get a print the statusz of a server.
+// usage:
+//  ./check host:999
+#include <google/protobuf/text_format.h>
+#include <grpc++/grpc++.h>
+#include <memory>
+#include "client.h"
+#include "hoist/errors.h"
+#include "hoist/init.h"
+#include "hoist/logging.h"
+
+Hoist::Result<std::shared_ptr<statusz::Status>> poll(std::string &addr) {
+  std::shared_ptr<grpc::Channel> channel =
+      grpc::CreateChannel(addr, grpc::InsecureChannelCredentials());
+  statusz::StatuszClient client(channel);
+  return client.Poll();
+}
+
+int main(int argc, char *argv[]) {
+  Hoist::Init();
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  if (argc != 2) {
+    std::cout << "Usage: \n" << argv[0] << " host:port" << std::endl;
+    return 1;
+  }
+
+  std::string addr(argv[1]);
+
+  ILOG("STATUSZ");
+  ILOG("Checking server " << addr);
+
+  Hoist::Result<std::shared_ptr<statusz::Status>> status = poll(addr);
+
+  if (!status.ok()) {
+    ELOG("Error checking " << addr << '.');
+  } else {
+    std::string s;
+    google::protobuf::TextFormat::PrintToString(*status.value(), &s);
+    ILOG("Got STATUSZ\n" << s);
+  }
+
+  google::protobuf::ShutdownProtobufLibrary();
+  return 0;
+}
